e021ea6a49c210e480e4a56ab84a47fe06ba23b1
David Falcone <s00271031@acad.tri-c.edu>
2019-11-22 20:33:02 -0500

    Created Lab5

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // 2) Identify the elements of a method header:
+         //    a) Scope is the range of the program that a variable can be used in.  Being public
+         //       allows access by everyone.
+         //    b) Static methods are classes provided to perform common tasks that do not 
+         //       require specific objects.
+         //    c) The return type is a double that is the outcome of multiplying height * width.
+         //    d) The method's name is getArea
+         //    e) This method expects two variables, a double height and a double width.
+         //    f) The method's body is defined by it's brackets {}.
+         
+         // 3) Method's that are provided with a framework are methods that have been extensively
+         //    debuged and optimized.  They are called using the form ClassName.MethodName(arguments)
+         //    A user defined method is on that you add yourself to your program.  For reliability
+         //    and ease of use it's most likely better to use methods from a framework.
+
+         // 4) A static method allows you to have direct access to the data from the method 
+         //    without having to create an object to recieve your data.
+
+
+      }
+   }
+}
