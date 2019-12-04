e8e2244a22c4b74ac5fa79c44e87c644c7dffddb
David Falcone <s00271031@acad.tri-c.edu>
2019-12-03 21:29:51 -0500

    Created Lab7

---
+++ Book/Book/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+   public class Book
+   {
+      public string Title { get; set; }
+      public string Author { get; set; }
+      public int Year { get; set; }
+
+      public string Display() =>
+         $"{"Title = ",10}: {this.Title}" +
+         $"\n{"Author = ",10}: {this.Author}";
+
+   }
+}
---
+++ Book/Book/BookTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+   class BookTest
+   {
+      static void Main()
+      {
+         string title = "Goode Book!";
+         string author = "Ireade Goode";
+         int year = 2019;
+
+         var book1 = new Book();
+
+         book1.Title = title;
+         book1.Author = author;
+         book1.Year = year;
+
+         Console.WriteLine($"{book1.Display()}");
+
+         Console.ReadLine();
+
+      }
+   }
+}
